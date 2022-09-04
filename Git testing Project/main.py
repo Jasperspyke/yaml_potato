ba06b62d0a3d6c391a41d2f555a314a080f8c6ce
Jasper Hilliard <marionjacobson@Marions-MacBook-Air.local>
2022-09-04 16:05:30 -0400

    ` ` Changet to be committed:

--- Git testing Project/main.py
+++ Git testing Project/main.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 #hyperparameters
 batch_size = 64
 
-#data loader 
+#data loader
 training_data = torchvision.datasets.mnist('mnist_data', train=True, download=True)
 val_dataset =  torchvision.datasets.mnist('mnist_data', train=False, download=True)
 
